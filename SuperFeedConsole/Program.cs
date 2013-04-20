bbd65749051d78fd29c10ad2411b931ab1bca1fb
Will Boyd <will@codersblock.com>
2013-04-20 17:06:49 -0400

    Added sample code to SuperFeedConsole to show how FeedCoordinator and FeedModules are used.

--- SuperFeedConsole/Program.cs
+++ SuperFeedConsole/Program.cs
@@ -11,6 +11,46 @@ namespace SuperFeedConsole
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Starting feed modules...");
+
+            FeedCoordinator.StartFeedModule(new TwitterFeedModule(80, "lonekorean"));
+            FeedCoordinator.StartFeedModule(new BloggerFeedModule(20, "codersblock"));
+            FeedCoordinator.StartFeedModule(new FlickrFeedModule(20, "44589846@N00", "lonekorean"));
+            FeedCoordinator.StartFeedModule(new DeviantArtFeedModule(20, "lonekorean"));
+
+            // the following module is commented out because it requires a private token
+            //FeedCoordinator.StartFeedModule(new InstagramFeedModule(20, "<token goes here>"));
+
+            while (true)
+            {
+                Console.Clear();
+                var list = FeedCoordinator.GetMergedAndBalancedFeed();
+                for (var i = 0; i < list.Count; i++)
+                {
+                    var item = list[i];
+                    Console.WriteLine("Item #" + (i + 1));
+                    Console.WriteLine(GetDisplayLine("SourceName", item.SourceName));
+                    Console.WriteLine(GetDisplayLine("Published", item.Published.ToString()));
+                    Console.WriteLine(GetDisplayLine("Title", item.Title));
+                    Console.WriteLine(GetDisplayLine("Snippet", item.Snippet));
+                    Console.WriteLine(GetDisplayLine("ImageThumbnailUri", item.ImageThumbnailUri));
+                    Console.WriteLine(GetDisplayLine("ImagePreviewUri", item.ImagePreviewUri));
+                    Console.WriteLine(GetDisplayLine("ViewUri", item.ViewUri));
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine("Press any key to refresh...");
+                Console.ReadKey(true);
+            }
+        }
+
+        static string GetDisplayLine(string label, string value)
+        {
+            label = "  " + (label + ":").PadRight(19);
+            value = value ?? "";
+            value = (value.Length <= 58 ? value : value.Substring(0, 55) + "...");
+
+            return label + value;
         }
     }
 }
